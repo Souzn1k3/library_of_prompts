79155380294d8c931b27bdce3649f37b2933fcb1
jakefmn <glebster.mc@gmail.com>
2026-04-10 16:26:44 +0300

    ui: remove profile scope navigation block

--- frontend/components/profile/ProfilePrimarySection.tsx
+++ frontend/components/profile/ProfilePrimarySection.tsx
@@ -4,7 +4,6 @@ import { useI18n } from "@/components/i18n/LanguageProvider";
 import { ProfileAccountOverview } from "@/components/profile/ProfileAccountOverview";
 import { ProfileActionsPanel } from "@/components/profile/ProfileActionsPanel";
 import { ProfileMembershipPanel } from "@/components/profile/ProfileMembershipPanel";
-import { ProfileNavScopeSection } from "@/components/profile/ProfileNavScopeSection";
 import type {
   BillingStatus,
   OnboardingProfile,
@@ -34,29 +33,25 @@ export function ProfilePrimarySection({
   const { t } = useI18n();
 
   return (
-    <>
-      <ProfileNavScopeSection />
+    <div className="grid gap-6 lg:grid-cols-[minmax(0,1.15fr)_minmax(0,0.85fr)]">
+      <ProfileAccountOverview
+        user={user}
+        summary={summary}
+        ratingLabel={ratingLabel}
+        locale={locale}
+        t={t}
+      />
 
-      <div className="grid gap-6 lg:grid-cols-[minmax(0,1.15fr)_minmax(0,0.85fr)]">
-        <ProfileAccountOverview
+      <div className="space-y-6">
+        <ProfileMembershipPanel
           user={user}
           summary={summary}
-          ratingLabel={ratingLabel}
+          billing={billing}
+          planUnlocks={planUnlocks}
           locale={locale}
-          t={t}
         />
-
-        <div className="space-y-6">
-          <ProfileMembershipPanel
-            user={user}
-            summary={summary}
-            billing={billing}
-            planUnlocks={planUnlocks}
-            locale={locale}
-          />
-          <ProfileActionsPanel onboardingProfile={onboardingProfile} />
-        </div>
+        <ProfileActionsPanel onboardingProfile={onboardingProfile} />
       </div>
-    </>
+    </div>
   );
 }
